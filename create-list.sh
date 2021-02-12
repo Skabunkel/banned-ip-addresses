fd7dd46d37b2aaf0dbc4fe0f52b2fa15e5de4904
Skabunkel <skabunkel@users.noreply.github.com>
2021-02-12 20:07:43 +0100

    fix: added better ip sorting

--- create-list.sh
+++ create-list.sh
@@ -3,5 +3,5 @@ NewIps=$(fail2ban-client status sshd | grep -oP "Banned IP list:\K.*" | xargs | 
 echo "$NewIps" > tmp.txt
 cat banned-ips.txt >> tmp.txt
 
-cat tmp.txt | sort | uniq > banned-ips.txt
+cat tmp.txt | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | uniq > banned-ips.txt
 rm tmp.txt
